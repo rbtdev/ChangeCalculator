a9d9d040c37eff288ca40dfedad119d73f2798b8
Rob Thuleen <rob.thuleen@gmail.com>
2016-11-14 21:19:20 -0800

    Added makeChange

--- index.js
+++ index.js
@@ -1,104 +1,75 @@
 
 
+function makeChange(amount) {
 
+	var cents = amount*100;
+	var change = {};
+
+	change.total = amount;
+	change.dollars = Math.floor(cents/100);
+	cents -= change.dollars*100;
+	change.quarters = Math.floor(cents/25)
+	cents -= change.quarters*25;
+	change.dimes =  Math.floor(cents/10)
+	cents -= change.dimes*10;
+	change.nickels = Math.floor(cents/5)
+	cents -= change.nickels*5;
+	change.pennies = Math.floor(cents);
+	console.log("Change amount = " + amount);
+	console.log("Change = " + JSON.stringify(change));
+	return change;
+}
 
 /* Grey background text in input boxes */
 function inputFocus(i){
-    if(i.value==i.defaultValue){ i.value=""; i.style.color="#000"; }
+	if(i.value==i.defaultValue){ i.value=""; i.style.color="#000"; }
 }
 function inputBlur(i){
-    if(i.value==""){ i.value=i.defaultValue; i.style.color="#888"; }
+	if(i.value==""){ i.value=i.defaultValue; i.style.color="#888"; }
 }
-	
+
 /* Round to decimal point */
 function roundToDecimal(num,dec) {
-      var rounded = (Math.round(num * Math.pow(10,dec)) / Math.pow(10,dec)).toFixed(dec);
-      return rounded;
-	  	  	  
-    }
+	var rounded = (Math.round(num * Math.pow(10,dec)) / Math.pow(10,dec)).toFixed(dec);
+	return rounded;
 
+}
 
+/*Bind a function to the click event */
+var button = document.getElementById('calculateChange');
 
- /*Bind a function to the click event */
-    var button = document.getElementById('calculateChange');
-
-		
 /* Field and Button setup */
 
-    button.onclick = function() {
-        
-        /* Cost of Purchase */
-        var costOfPurchase = document.getElementById('costOfPurchase');
+button.onclick = function() {
 
-        var cost = costOfPurchase.value;
+	/* Cost of Purchase */
+	var costOfPurchase = document.getElementById('costOfPurchase');
 
-        /* Amount given by customer */
-		var amountTendered = document.getElementById('amountTendered');
+	var cost = costOfPurchase.value;
 
-        var amount = amountTendered.value;
+	/* Amount given by customer */
+	var amountTendered = document.getElementById('amountTendered');
 
-        
-/* Initial Change Calculation */
-        
-		var change = roundToDecimal(amount - cost, 2);
-		/* console.log(change); */
-		 document.getElementById('totalOutput').innerHTML = 'Your Change Is: $' + (change);
-		console.log(change);
-		
+	var amount = amountTendered.value;
 
 
-/*var calculateChange = document.getElementById('calculateChange'); */
+	/* Initial Change Calculation */
 
+	var change = makeChange(roundToDecimal(amount - cost, 2));
+	/* console.log(change); */
 
-/* DOLLARS WHOLE VALUE*/
-document.getElementById('dollars').innerHTML = 'Dollars: $' + Math.floor(change);
-
-var dollars = Math.floor(change);
-console.log(dollars);
-
-/* Quarters Calculations */
-
-var quarters = (Math.floor((change - dollars) / .25));
-console.log(quarters);
-document.getElementById('quarters').innerHTML = 'Quarters: ' + quarters;
-
-/* Dime Calculations */
+	document.getElementById('totalOutput').innerHTML = 'Your Change Is: $' + (change.total);
+	console.log(change.dollars);
 
 
 
-
-/*Whole value - Rounded Value  
-var wholeChangeValue = (change - Math.floor(change) );
-console.log(wholeChangeValue);
-
-var quarters = (Math.round(wholeChangeValue / .25));
-console.log(quarters);
-
-document.getElementById('quarters').innerHTML = 'Quarters: ' + quarters;
-
-console.log (wholeChangeValue); */
+	document.getElementById('dollars').innerHTML = 'Dollars: $' + change.dollars;
+	document.getElementById('quarters').innerHTML = 'Quarters: ' + change.quarters;
+	document.getElementById('dimes').innerHTML = 'Dimes: ' + change.dimes;
+	document.getElementById('nickels').innerHTML = 'Nickles: ' + change.nickels;
+	document.getElementById('pennies').innerHTML = 'Pennies: ' + change.pennies;
 
 
-
-
-
-
-
-var dimes = "";
-var nickels = "";
-var pennies = "";
-
-
-
-	
-
- /* Assign the message to the output element we just fetched from the DOM */
-        
-		document.getElementById('calculateChange');
-		
-		       
-		
-		
-    }
+}
 
 
